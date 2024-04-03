78c5348307d5ec939807242a4958119b92470023
Heidi Fryzell <heidi37@users.noreply.github.com>
2024-04-03 12:10:31 -0400

    adds multiple images and the ability to select by breed

--- app.js
+++ app.js
@@ -1,18 +1,44 @@
-const dogImage = document.querySelector('img');
-const newPicButton = document.querySelector('button')
+const dogImages = document.querySelectorAll('img');
+const newPicButton = document.querySelector('button');
+const selectedBreed = document.querySelector('.dog-selector')
 
-newPicButton.addEventListener("click", function() {
+window.addEventListener('load', getBreeds)
+newPicButton.addEventListener("click", getImages)
+
+function getBreeds(){
   const XHR = new XMLHttpRequest();
+  XHR.open("GET", 'https://dog.ceo/api/breeds/list/all');
+  XHR.send();
+  XHR.onreadystatechange = function(){
+    if(XHR.readyState == 4 && XHR.status == 200){
+      const response = JSON.parse(XHR.responseText)
+      Object.keys(response.message).forEach(breed => {
+      const option = document.createElement('option');
+      option.value = breed;
+      option.textContent = breed.toUpperCase();
+      selectedBreed.appendChild(option);
+    });
+    } else if (XHR.readyState == 4) {
+      console.log("There is a problem")
+    }
+  }
+}
 
-  XHR.open("GET", "https://dog.ceo/api/breeds/image/random");
+function getImages() {
+  for (let image of dogImages) {
+  const XHR = new XMLHttpRequest();
+  XHR.open("GET", `https://dog.ceo/api/breed/${selectedBreed.value}/images/random`);
   XHR.send();
   XHR.onreadystatechange = function(){
     if(XHR.readyState == 4 && XHR.status == 200){
       const response = JSON.parse(XHR.responseText)
       console.log(response)
-      dogImage.setAttribute("src", response.message)
+      image.style.background = `url(${response.message})`;
+      image.style.backgroundRepeat = 'no-repeat';
+      image.style.backgroundSize = "cover"
     } else if (XHR.readyState == 4) {
       console.log("There is a problem")
     }
   }
-})
+}
+}
